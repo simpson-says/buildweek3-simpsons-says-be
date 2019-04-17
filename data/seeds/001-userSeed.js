476a4e678390c7761c242f992a6625f1cc48f47c
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-04-17 02:31:43 -0700

    Heroku fix

--- data/seeds/001-userSeed.js
+++ data/seeds/001-userSeed.js
@@ -1,15 +1,10 @@
 const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('users').insert([
         {username: 'omar', password: bcrypt.hashSync("password", 10), role: "admin"},
         {username: 'andrew', password: bcrypt.hashSync("password", 10), role: "user"},
         {username: 'john', password: bcrypt.hashSync("password", 10), role: "user"},
         {username: 'basil', password: bcrypt.hashSync("password", 10), role: "user"}
       ]);
-    });
 };
